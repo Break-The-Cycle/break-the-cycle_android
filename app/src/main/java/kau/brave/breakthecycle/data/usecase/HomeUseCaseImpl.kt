ac510b7c533e9f030807c6bfaaeb1d5d61959da4
dlgocks1 <cksgodl1@naver.com>
2023-06-01 13:32:59 +0900

    feat : homeUseCase 추가

---
+++ app/src/main/java/kau/brave/breakthecycle/data/usecase/HomeUseCaseImpl.kt
@@ -0,0 +1,8 @@
+package kau.brave.breakthecycle.data.usecase
+
+import kau.brave.breakthecycle.domain.usecase.HomeUseCase
+import javax.inject.Inject
+
+class HomeUseCaseImpl @Inject constructor() : HomeUseCase {
+    
+}
---
+++ app/src/main/java/kau/brave/breakthecycle/domain/usecase/HomeUseCase.kt
@@ -0,0 +1,4 @@
+package kau.brave.breakthecycle.domain.usecase
+
+interface HomeUseCase {
+}
